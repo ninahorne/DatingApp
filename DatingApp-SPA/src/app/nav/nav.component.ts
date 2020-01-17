a914c917a4d7ac0723a6d6efbff0e9c296eabc1b
ninahorne <nina.horne1221@gmail.com>
2020-01-17 15:55:42 -0600

    End of Section 7

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/Auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -12,7 +13,8 @@ export class NavComponent implements OnInit {
 
   constructor(
     public authService: AuthService,
-    private alertify: AlertifyService
+    private alertify: AlertifyService,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -21,10 +23,13 @@ export class NavComponent implements OnInit {
 
   login() {
     this.authService.login(this.model).subscribe(next => {
-      this.alertify.success('logged in successfully');
+      this.alertify.success('Logged in successfully');
     },
       error => {
         this.alertify.error(error);
+      },
+      () => {
+        this.router.navigate(['/members']);
       }
     );
   }
@@ -36,5 +41,6 @@ export class NavComponent implements OnInit {
   logout() {
     localStorage.removeItem('token');
     this.alertify.message('logged out');
+    this.router.navigate(['/home'])
   }
 }
---
+++ DatingApp-SPA/src/app/routes.ts
@@ -0,0 +1,19 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AuthGuard } from './_guards/auth.guard';
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
+
+export const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    {
+        path: '', runGuardsAndResolvers: 'always', canActivate: [AuthGuard], children: [
+            { path: 'members', component: MemberListComponent },
+            { path: 'messages', component: MessagesComponent },
+            { path: 'lists', component: ListsComponent }
+        ]
+    },
+    { path: '**', redirectTo: '', pathMatch: 'full' }
+];
